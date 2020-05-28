2c64f57da1fe583c69b47b4722ea142c4e705c6a
Hiroshi Fujiwara <fujiwara@no1s.biz>
2020-05-28 01:55:32 +0000

    fix chapter05 for master

--- chapter05/work01.js
+++
@@ -1,5 +0,0 @@
-// 1円周率
-console.log(Math.PI);
-
-// 4 * PI
-console.log(4 * Math.PI);
--- chapter05/work04.js
+++
@@ -1,4 +0,0 @@
-// 乱数の発生（1から9）
-for(var i = 0; i < 10; i++) {
-    console.log(Math.floor(Math.random() * 10));
-}
